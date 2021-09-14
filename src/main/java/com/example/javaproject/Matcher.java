264cb81f0f359edbe2bb57c3f9000f9caf87e320
jodielaurenson <jlaurenson@scottlogic.com>
2021-09-14 16:48:08 +0100

    Converted Arrays to Lists

--- src/main/java/com/example/javaproject/Matcher.java
+++ src/main/java/com/example/javaproject/Matcher.java
@@ -7,23 +7,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Matcher {
-	private Order[] buyOrders = {};
-	private Order[] sellOrders = {};
 
-	private Order[] insertOrder(Order[] orders, int account, float price, int quantity, actionType action){
-		//Insert order in the correct position
-		orders = Arrays.copyOf(orders, orders.length+1);
-		int length = orders.length;
-		orders[length-1] = new Order(account, price, quantity, action);
-		return orders;
-	}
+	private List<Order> buyOrders = new ArrayList<Order>();
+	private List<Order> sellOrders = new ArrayList<Order>();
 
 	public void addNewOrder(int account, float price, int quantity, actionType action){
 		if(action==actionType.BUY){
-			this.buyOrders = insertOrder(this.buyOrders, account, price, quantity, action);
+			this.buyOrders.add(new Order(account, price, quantity, action));
 		}
 		else {
-			this.sellOrders = insertOrder(this.sellOrders, account, price, quantity, action);
+			this.sellOrders.add(new Order(account, price, quantity, action));
 		}
 	}
 }
