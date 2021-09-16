d542f383e21f3d05fcdccb31c6b168085bcfa9fb
jodielaurenson <jlaurenson@scottlogic.com>
2021-09-16 15:51:58 +0100

    Removed unused fields

--- src/main/java/com/example/javaproject/Trade.java
+++ src/main/java/com/example/javaproject/Trade.java
@@ -14,10 +14,7 @@ public class Trade {
     private  Order newOrder;
     private float price;
     private int quantity;
-    private String date;
-    private String time;
     private Date datetime = new Date();
-
 
     public Trade(Order oldOrder, Order newOrder) {
         this.oldOrder = oldOrder;
